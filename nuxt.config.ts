c9d209151751784612c3f1163c33f0597deea99d
shalaginoid@mail.ru <shalaginoid@mail.ru>
2026-03-29 14:55:47 +0500

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+
+  modules: ['@nuxt/ui'],
+
+  app: {
+    baseURL: '/elite-bird/',
+  },
+
+  nitro: {
+    preset: 'github-pages',
+  },
+});
