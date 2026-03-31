59b8fad9cadc8b7548426c052ca9805acf4cdb4b
shalaginoid@mail.ru <shalaginoid@mail.ru>
2026-03-31 09:10:44 +0500

    commit

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,8 @@
     <LandingHero />
 
     <Catalog />
+
+    <pre>{{ users }}</pre>
   </UPage>
 </template>
 
@@ -10,5 +12,11 @@
 useSeoMeta({
   title: 'Перепела бройлерные, цесарка, фазан вольерный - купить в Екатеринбурге',
   description: 'Производим мясо элитной птицы и бесплатно доставляем по Екатеринбургу',
+});
+
+const users = ref(null);
+
+onMounted(async () => {
+  users.value = await $fetch('https://jsonplaceholder.typicode.com/users');
 });
 </script>
