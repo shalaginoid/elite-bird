e1ad882bfe9a34fd1cd46da9478d2981ea0e7c91
shalaginoid@mail.ru <shalaginoid@mail.ru>
2026-04-01 19:40:37 +0500

    commit

--- app/components/landing/Delivery.vue
+++ app/components/landing/Delivery.vue
@@ -1,10 +1,32 @@
 <template>
   <UPageSection title="Доставка и оплата">
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor velit corrupti minima quam error incidunt magni
-      quibusdam! Nihil expedita rem sint ex consectetur modi voluptatem doloribus, consequatur unde error deleniti!
-    </p>
+    <Motion
+      v-for="(item, index) in items"
+      :key="index"
+      :initial="{ opacity: 0, transform: 'translateY(20px)' }"
+      :while-in-view="{ opacity: 1, transform: 'translateY(0)' }"
+      :transition="{ delay: 0.4 + 0.2 * Number(index) }"
+      :in-view-options="{ once: true }"
+      class="text-muted flex items-center text-nowrap gap-2"
+    >
+      <UPageFeature v-bind="item" />
+    </Motion>
   </UPageSection>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { PageFeatureProps } from '@nuxt/ui';
+
+const items: PageFeatureProps[] = [
+  {
+    title: 'Доставка',
+    description: 'Доставка осуществляется только по Екатеринбургу. Точная дата будет определена после обработки заказа',
+    icon: 'i-lucide-map-pin',
+  },
+  {
+    title: 'Оплата',
+    description: 'Оплата осуществляется после получения заказа наличными или переводом на карту',
+    icon: 'i-lucide-credit-card',
+  },
+];
+</script>
