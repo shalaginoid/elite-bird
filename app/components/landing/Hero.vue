af42cdb3dd77610cfe2309a183b6afda756efb52
shalaginoid@mail.ru <shalaginoid@mail.ru>
2026-04-01 19:25:26 +0500

    commit

--- app/components/landing/Hero.vue
+++ app/components/landing/Hero.vue
@@ -45,9 +45,11 @@
           delay: 0.3,
         }"
       >
-        Производим и бесплатно доставляем
-        <br />
-        по Екатеринбургу
+        <span>
+          Производим и бесплатно доставляем
+          <br />
+          по Екатеринбургу
+        </span>
       </Motion>
     </template>
 
@@ -70,53 +72,8 @@
       >
         <div v-if="links" class="flex items-center gap-2">
           <UButton v-bind="links[0]" />
-
-          <!-- <UButton
-            :color="global.available ? 'success' : 'error'"
-            variant="ghost"
-            class="gap-2"
-            :to="global.available ? global.meetingLink : ''"
-            :label="global.available ? 'Available for new projects' : 'Not available at the moment'"
-          >
-            <template #leading>
-              <span class="relative flex size-2">
-                <span
-                  class="absolute inline-flex size-full rounded-full opacity-75"
-                  :class="global.available ? 'bg-success animate-ping' : 'bg-error'"
-                ></span>
-
-                <span
-                  class="relative inline-flex size-2 scale-90 rounded-full"
-                  :class="global.available ? 'bg-success' : 'bg-error'"
-                ></span>
-              </span>
-            </template>
-          </UButton> -->
         </div>
       </Motion>
-
-      <!-- <div class="gap-x-4 inline-flex mt-4">
-        <Motion
-          v-for="(link, index) of footer?.links"
-          :key="index"
-          :initial="{
-            scale: 1.1,
-            opacity: 0,
-            filter: 'blur(20px)',
-          }"
-          :animate="{
-            scale: 1,
-            opacity: 1,
-            filter: 'blur(0px)',
-          }"
-          :transition="{
-            duration: 0.6,
-            delay: 0.5 + index * 0.1,
-          }"
-        >
-          <UButton v-bind="{ size: 'md', color: 'neutral', variant: 'ghost', ...link }" />
-        </Motion>
-      </div> -->
     </template>
 
     <UMarquee pause-on-hover class="py-2 -mx-8 sm:-mx-12 lg:-mx-16 [--duration:40s]">
